16979349aaf111a601c53d73c2b8b3f9fcc39bc3
Diego Saldonid <dsaldoni@ucsd.edu>
2021-06-10 12:13:42 -0700

    just need flexbox component

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const limit   = 9;
 const rating  = "g";
 let   movieId;
 const movieForm = document.querySelector("form");
-const loadButton = document.querySelector(".hidden");
+const loadButton = document.querySelector("#loadMore");
 const photoSize  = "w200"
 let   secureURL;
 let   movieUrls = [];
@@ -13,11 +13,11 @@ let   movieInfo = {};
 let   page =1;
 
 
-movieForm.addEventListener("submit", getResults)
+movieForm.addEventListener("submit", filter)
+loadButton.addEventListener("click",loadMore)
 
-async function getResults(evt){
-    evt.preventDefault()
-
+async function getResults(){
+    // evt.preventDefault()
     //grab secure base URL
     let conFigURL = `https://api.themoviedb.org/3/configuration?api_key=${apiKey}`;
     const response     = await fetch(conFigURL);
@@ -33,7 +33,6 @@ async function getResults(evt){
 
     //store movie info in object
     responseData1.results.map(storeInfo);
-    console.log(movieInfo);
     
     // // get data on dom 
 
@@ -46,11 +45,29 @@ async function getResults(evt){
     page++;
 }
 
-function filter(){
+async function filter(event){
+    //prevent reloading page when something entered
+    event.preventDefault()
+
     //wipe page
-    setTimeout(wipeResults,5000)
+    setTimeout(wipeResults,0)
+
+    //grab search term
+    const searchName      = event.target.movie;
+    const movie           = searchName.value;
+    const year            = 2021
+
+    //make api call to filter movies
+    const searchURL    = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${movie}&primary_release_year=${year}`;
+    const response     = await fetch(searchURL);
+    const responseData = await response.json();
+
+    
+    //display the filtered titles
+    responseData.results.map(displayResults)
 
 }
+
 function storeInfo(movieData){
     //save our movie's title,average moving votes, and picture's URL
     let movie_pic    = secureURL + photoSize+ movieData.poster_path
@@ -67,12 +84,36 @@ function displayResults(movieData){
     let movie_pic = secureURL + photoSize+ movieData.poster_path
     // console.log(gifData.url)
     movieArea.innerHTML+= `
-    <img src = "${movie_pic}">
+    <img src = "${movie_pic}" alt = "${movieData.title}">
     <h4>${movieData.title}</h4>
+    <img src = "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/320/apple/285/star_2b50.png" class = "star" alt = "stars">
+    <h4>${movieData.vote_average}
     `
 }
 
 function wipeResults(){
     //wipe out movieArea
     movieArea.innerHTML = ''
+}
+
+window.onload = function(){
+    getResults()
+}
+
+async function loadMore(){
+    //loop through every page and add info to data structures
+    let apiURL       = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&page=${page}`;
+
+    // // go to movie api 
+    let response     = await fetch(apiURL);
+    let responseData = await response.json();
+
+    //store movie info in object
+    responseData.results.map(storeInfo);
+
+    // // get data on dom 
+    responseData.results.map(displayResults)
+
+    //increment page
+    page++;
 }
